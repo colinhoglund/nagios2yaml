f00c618abc03a42d7b1db2c44502546cf46c59fb
Colin Hoglund <colinhoglund@gmail.com>
2015-09-05 16:27:12 -0400

    adding comments

--- n2y.py
+++ n2y.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+'''
+Tool for converting Nagios object definitions
+  to YAML format.
 
+Usage: ./n2y.py <filename>
+'''
 from sys import argv
 import re
 
 script, filename = argv
-
 text = open(filename)
 
+# Create a dictionary of lists to
+#   store Nagios objects definitions
 objects = {
 'commands': [],
 'contacts': [],
@@ -19,26 +25,35 @@ objects = {
 'timeperiods': [],
 }
 
-obj = {}
+temp_obj = {}
 obj_type = ''
-
 for line in text.readlines():
+    # remove comments
     if '#' in line:
         continue
     if ';' in line:
         line = re.sub(';.*$', '', line)
+    # start building Nagios object
     if '{' in line:
+        # create white space around curly
+        #   braces for string splitting
         line = re.sub('{', ' { ', line)
         obj_type = line.split()[1] + 's'
         continue
+    # finish building Nagios object
     if '}' in line:
         line = re.sub('}', ' } ', line)
-        objects.get(obj_type).append(obj)
-        obj = {}
+
+        # write temp_obj to objects
+        objects.get(obj_type).append(temp_obj)
+        temp_obj = {}
         obj_type = ''
         continue
+    # skip line if { hasn't been encountered
     if not obj_type:
         continue
-    obj[line.split()[0]] = ' '.join(line.split()[1:])
+
+    # update temp_obj
+    temp_obj[line.split()[0]] = ' '.join(line.split()[1:])
 
 print objects
